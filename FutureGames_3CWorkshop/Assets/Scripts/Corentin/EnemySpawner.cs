f1a8294d9a056787d2b9afd247175efea70318ea
Corentin.Haot <Corentin.haot@yahoo.be>
2022-10-07 14:35:19 +0200

    Added Spawning enemies script & Enemy prefab

---
+++ FutureGames_3CWorkshop/Assets/Scripts/Corentin/EnemySpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+
+    [SerializeField] private float enemyInterval = 3.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(enemyInterval, enemyPrefab));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), 0, Random.Range(-6f, 6f)), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+}
